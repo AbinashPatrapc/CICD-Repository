c1ae85b4f84f2755a3645fd05ec742282bdcea3a
Abinash Patra <abinash.patra@nwbrd.com>
2024-09-23 13:20:41 +0530

    added time in wish api

--- CICD-TestingProject/src/main/java/com/cicd/controller/Controller.java
+++ CICD-TestingProject/src/main/java/com/cicd/controller/Controller.java
@@ -18,18 +18,19 @@ public class Controller {
 	@GetMapping("/wish")
 	public ResponseEntity<String> getWishMessage(){
 		LocalDateTime time=LocalDateTime.now();
+		
 		int hour=time.getHour();
 		String message;
 		if(hour<12) {
-			message="<h1 style='color:green;text-align:center'>Good Moring </h1>";
+			message="<h1 style='color:green;text-align:center'>Good Moring<br>Current Time: "+time+"</h1>";
 		}else if(hour==12) {
-			message="<h1 style='color:red;text-align:center'>Good Noon </h1>";
+			message="<h1 style='color:red;text-align:center'>Good Noon<br>Current Time: "+time+"</h1>";
 		}else if(hour>12 && hour<18) {
-			message="<h1 style='color:green;text-align:center'>Good Afternoon </h1>";
+			message="<h1 style='color:green;text-align:center'>Good Afternoon<br>Current Time: "+time+"</h1>";
 		}else if(hour>=18 && hour<21) {
-			message="<h1 style='color:orange;text-align:center'>Good Evening </h1>";
+			message="<h1 style='color:orange;text-align:center'>Good Evening<br>Current Time: "+time+"</h1>";
 		}else {
-			message="<h1 style='color:magenta;text-align:center'>Good Night </h1>";
+			message="<h1 style='color:magenta;text-align:center'>Good Night<br>Current Time: "+time+"</h1>";
 		}
 		return new ResponseEntity<>(message,HttpStatus.OK);
 	}
