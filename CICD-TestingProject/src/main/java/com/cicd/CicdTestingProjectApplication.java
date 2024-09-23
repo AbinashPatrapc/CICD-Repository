638827394fcf2c24105fbaa4370922d126296085
Abinash Patra <abinash.patra@nwbrd.com>
2024-09-23 13:07:56 +0530

    new api added

--- CICD-TestingProject/src/main/java/com/cicd/CicdTestingProjectApplication.java
+++ CICD-TestingProject/src/main/java/com/cicd/CicdTestingProjectApplication.java
@@ -8,6 +8,7 @@ public class CicdTestingProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdTestingProjectApplication.class, args);
+		System.out.println("Application Started");
 	}
 
 }
--- CICD-TestingProject/src/main/java/com/cicd/controller/Controller.java
+++ CICD-TestingProject/src/main/java/com/cicd/controller/Controller.java
@@ -1,5 +1,9 @@
 package com.cicd.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +12,25 @@ public class Controller {
 
 	@GetMapping
 	public String getMessage() {
-		System.out.println("Api Triggered");
 		return "<h1 style='color:red;text-align:center'>Welcome to Netweb</h1>";
+	}
+	
+	@GetMapping("/wish")
+	public ResponseEntity<String> getWishMessage(){
+		LocalDateTime time=LocalDateTime.now();
+		int hour=time.getHour();
+		String message;
+		if(hour<12) {
+			message="<h1 style='color:green;text-align:center'>Good Moring </h1>";
+		}else if(hour==12) {
+			message="<h1 style='color:red;text-align:center'>Good Noon </h1>";
+		}else if(hour>12 && hour<18) {
+			message="<h1 style='color:green;text-align:center'>Good Afternoon </h1>";
+		}else if(hour>=18 && hour<21) {
+			message="<h1 style='color:orange;text-align:center'>Good Evening </h1>";
+		}else {
+			message="<h1 style='color:magenta;text-align:center'>Good Night </h1>";
+		}
+		return new ResponseEntity<>(message,HttpStatus.OK);
 	}
 }
